a332eabee75d21a1bf0fde2f8937c50ad961a37d
Pedro <pedrohenrique010123456@gmail.com>
2026-04-29 21:25:16 -0300

    adiciona a verificação de token nas rotas

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { PostController } from "../controller/PostController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 const postController = new PostController();
 
 router.get("/", postController.list);
-router.post("/", postController.create);
-router.patch("/:id", postController.update);
-router.delete("/:id", postController.delete);
+router.post("/", authMiddleware, postController.create);
+router.patch("/:id", authMiddleware, postController.update);
+router.delete("/:id", authMiddleware, postController.delete);
 
 export const postRoutes = router;
